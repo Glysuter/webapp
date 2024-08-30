ef62c0f0e72c5f8bb647bae0cab690d9d0e74da1
Glysuter <sutergloria@gmail.com>
2024-08-30 11:52:48 -0500

    modify

--- test.java
+++ test.java
@@ -1,3 +1,7 @@
-functional testin
-functional testin development
-functional testin development
+Testing by the QA team
+Testing web apps before deployment
+  running unit testing
+functional testing
+functional testing development
+functional testing
+functional testing development
